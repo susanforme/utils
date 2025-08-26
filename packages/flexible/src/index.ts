125d880fbe835b1e18c05108a3036cc1242620f0
susanforme <rzc307853639@gmail.com>
2025-08-26 15:30:16 +0800

    fix: 修复viewport宽度计算逻辑，添加相关参考链接

--- packages/flexible/src/index.ts
+++ packages/flexible/src/index.ts
@@ -143,6 +143,7 @@ export const flexible = (options: FlexibleOptions = {}): (() => void) => {
     // 内部核心计算逻辑，可以被重复调用
     const recalculate = () => {
       //FIX: meta viewport 设置的 width 为 device-width 时，window.innerWidth 会比 window.visualViewport?.width 大
+      // https://stackoverflow.com/questions/36297612/window-innerwidth-in-chromes-device-mode
       const viewportWidth = window.visualViewport?.width ?? window.innerWidth; // 用于断点匹配
       // const effectiveWidth = document.documentElement.clientWidth; // 用于rem计算
       //TODO: ?没办法去判断滚动条的状态
